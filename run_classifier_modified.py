fcad46e5fb491ee36d182f4f5a33041e757dd1b8
explorer_DL <31173861+PExplorer@users.noreply.github.com>
2018-12-25 18:03:46 +0530

    Update run_classifier_modified.py

--- run_classifier_modified.py
+++ run_classifier_modified.py
@@ -576,6 +576,7 @@ def _truncate_seq_pair(tokens_a, tokens_b, max_length):
 def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
                  labels, num_labels, use_one_hot_embeddings):
   """Creates a classification model."""
+  import modeling
   model = modeling.BertModel(
       config=bert_config,
       is_training=is_training,
